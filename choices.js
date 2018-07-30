4c9c41d24cd329f652981b660d2ffca7df6a4f68
Elena Granados <patty518@yahoo.com>
2018-07-30 14:41:17 -0700

    add correct quiz files and delete replit_main file

---
+++ choices.js
@@ -0,0 +1,111 @@
+const CHOICE = [
+ {
+   page: 1,
+   question: "What is a black hole?",
+  answers: [
+      "Facebook",
+      "My wallet",
+      "A place in space where gravity pulls so much that even light cannot get out.",
+      "A dark place where all things that have ever been lost reside."],
+   correctAnswer: "2"
+},
+
+ {
+   page: 2,
+   question: "What is a quasar?",
+   answers:[
+      "A delicious chocolate bar with caramel and nougat.",
+      "A distant object powered by black holes a billion times as massive as our sun.",
+      "A negatively charged quark that rotates the opposite way of everthing around it.",
+      "Galaxies that are pulling objects toward them in order to swallow them whole."],
+   correctAnswer: "1"
+ },
+
+ {
+   page: 3,
+   question: "What is a galaxy?",
+   answers: [
+      "The soccer team in Los Angeles",
+      "A distant object powered by black holes a billion times as massive as our sun.",
+      "A place in space where gravity pulls so much that even light can not get out.",
+      "A system of millions or billions of stars, together with gas and dust, held together by gravitational attraction."],
+   correctAnswer: "3"
+ },
+
+ {
+   page: 4,
+   question: "What is a planet?",
+   answers: [
+      "A round or near round object that orbits the sun, and which has removed debris or other small objects from the area around its orbit.",
+      "A satellite that orbits the sun.",
+      "A large collection of gases and dust that orbits a gallaxy.",
+      "A fixed luminous point in the night sky that is a large, remote incandescent body like the sun."],
+   correctAnswer: "0"
+ },
+
+ {
+   page: 5,
+   question: "What is a star?",
+   answers: [
+      "Star is an American musical drama television series created by Lee Daniels and Tom Donaghy for Fox. It revolves around three talented young singers.",
+      "A fixed luminous point in the night sky that is a large, remote incandescent body like the sun.",
+      "A small body of matter from outer space that enters the earth's atmosphere, becoming incandescent as a result of friction and appearing as a streak of light.",
+      "A round or near round object that orbits the sun, and which has revomed debris or other small objects from the area around its orbit."],
+   correctAnswer: "1"
+ },
+
+ {
+   page: 6,
+   question: "What is the Big Bang theory?",
+   answers: [
+      "A theory that claims the universe was created by an old dude in the sky in 7 days.",
+      "A theory that claims the universe originated billions of years ago in an explosion from a single point of nearly infinite energy density.",
+      "A show about some cute astrophysicists who work at Caltech who have trouble dating because they are so smart.",
+      "A theory that claims all living creatures originally evolved from bacteria."],
+   correctAnswer: "1"
+ },
+
+ {
+   page: 7,
+   question: "What is a supernova?",
+   answers: [
+      "A hitsong from the band Oasis that references crashing on champagne.",
+      "A fixed luminous point in the night sky that is a large, remote incandescent body like the sun.",
+      "A star that suddenly increases greatly in brightness because of a catastrophic explosion that ejects most of its mass.",
+      "A round or near round object that orbits the sun, and which has revomed debris or other small objects from the area around its orbit."],
+   correctAnswer: "2"
+ },
+
+ {
+   page: 8,
+   question: "What is a dark matter?",
+   answers: [
+      "A theorized form of matter that is believed to account for approximately 80% of the matter in the universe, and about a quarter of its total energy density.",
+      "A place in space where gravity pulls so much that even light can not get out.",
+      "A form of energy which is hypothesized to permeate all of space, tending to accelerate the expansion of the universe, and which makes up nearly three-fourths of the universe.",
+      "A dark place where all things that have ever been lost reside."],
+   correctAnswer: "0"
+ },
+
+ {
+   page: 9,
+   question: "What is dark energy?",
+   answers: [
+      "A theorized form of matter that is believed to account for approximately 80% of the matter in the universe, and about a quarter of its total energy density.",
+      "An aspect of the Force, a metaphysical power that bound the galaxy together. The first known users of the dark side were the ancient Rakata, an alien species that conquered a large portion of the galaxy around 30,000 BBY.",
+      "An energy at the center of black holes where gravity pulls so much that even light can not get out.",
+      "A form of energy which is hypothesized to permeate all of space, tending to accelerate the expansion of the universe, and which makes up nearly three-fourths of the universe."],
+   correctAnswer: "3"
+ },
+
+ {
+   page: 10,
+   question: "The Universe: ",
+   answers: [
+      "is expanding at an increasing rate.",
+      "is expanding at a decreasing rate.",
+      "is contracting at an increasing rate.",
+      "is contracting at a decreasing rate."],
+   correctAnswer: "0"
+ }
+]
--- index.js
+++ index.js
@@ -1,63 +1,131 @@
-const GITHUB_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+'use strict'
 
-function getDataFromApi(searchTerm, callback) {
-	console.log(`display searchTerm from getDataFromApi ${searchTerm}`);
-	const query = {
-		part: 'snippet',
-		key: 'AIzaSyDE2RS2B27KuUp-G6TWpRFtLpySC36Zf3c',
-		q: `${searchTerm} in:name`,
-	};
-	$.getJSON(GITHUB_SEARCH_URL, query, callback);
+let indexNumber = 0;
+let score=0;
+
+// generate the quiz item for the form
+function generateItemElementString(item) {
+  console.log("`generateItemString` ran")
+  console.log(item);
+
+  let output = [`
+  <legend>Question ${item.page} out of 10: ${item.question}</legend>`];
+
+    // Object.keys(item.answers).forEach(function(key) {
+    //   output.push(`<button type="submit" name="option" class="option" value= "${key}">${item.answers[key]}</button>`);
+    // });
+
+  for (let i=0; i<item.answers.length; i++) {
+    output.push(`<button type="submit" name="option" class="option" value= "${i}"><span class="darken">${item.answers[i]}</span></button>`);
+  };
+
+  console.log(output);
+  return output;
 }
 
-function renderResult(result, index) {
-	console.log(result);
-	return `
-    <div class="image">
-     <a href="https://www.youtube.com/watch?v=${result.id
-				.videoId}" target="_blank">
-      <img src="${result.snippet.thumbnails.medium.url}"
-     alt="${result.snippet.title}" data-index="${index}"></a>
-     <p><a href="https://www.youtube.com/channel/${result.snippet
-				.channelId}" target="_blank">more from this channel </a></p>
-     </div>
-   
-    `;
+// render each question and answer choices
+function renderQuestion() {
+  console.log("`renderQuestion` ran");
+  if (indexNumber<CHOICE.length) {
+  const quizItemString = generateItemElementString(CHOICE[indexNumber]);
+  // insert that HTML in to the DOM
+  $('.questions').show();
+  $('.quiz-content').html(quizItemString);
+  } else {
+    finalResult();
+  };
 }
 
-function renderLightBox(event) {
-	$('.js-search-results').on('click', 'img', event => {
-		console.log('renderLightBox has ran');
-		let ind = $('.js-search-results')
-			.find(event.currentTarget)
-			.attr('data-index');
-
-		console.log(ind);
-	});
+function correctAnswer() {
+  console.log("correctAnswer ran")
+  score++;
+  $('.questions').hide();
+  $('.feedback').show().html(`
+    <h2>You are correct!</h2>
+    <p>Score: ${score}</p>
+    <button class="next">next</button>`);
 }
 
-function displayGitHubSearchData(data) {
-	const results = data.items.map((item, index) => renderResult(item, index));
-	console.log(`display results ${results}`);
-
-	$('.js-search-results').html(results);
+function wrongAnswer(text) {
+  console.log("wrongAnswer ran")
+  $('.questions').hide();
+  $('.feedback').show().html(`
+    <h2>Wrong Answer!</h2>
+  <p class="correct">The correct answer is: ${text}</p>
+  <p>Score: ${score}</p>
+  <button class="next">next</button>`);
 }
 
-function watchSubmit() {
-	$('.js-search-form').submit(event => {
-		event.preventDefault();
-		const queryTarget = $(event.currentTarget).find('.js-query');
-		const query = queryTarget.val();
-		console.log(`watch submit function has a search for ${query}`);
-		// clear out the input
-		queryTarget.val('');
-		getDataFromApi(query, displayGitHubSearchData);
-	});
+// evaluate if selected answer matches correct answer
+function evaluateAnswer(selected) {
+  console.log('`evaluateAnswer` ran');
+  let correctAnswerIndex=CHOICE[indexNumber].correctAnswer;
+  let correctAnswerText=CHOICE[indexNumber].answers[correctAnswerIndex];
+  console.log(`the selected answer value is ${selected} and the correct answer index is ${correctAnswerIndex} and the correct answer text is ${correctAnswerText}`);
+  indexNumber++;
+  $('.feedback').removeClass('hidden');
+
+  if(correctAnswerIndex==selected) {
+    correctAnswer();
+  } else {
+    wrongAnswer(correctAnswerText);
+  }
+
 }
 
-function start() {
-	renderLightBox();
-	watchSubmit();
+// at the end, you should see your result and restart quiz
+function finalResult() {
+  console.log('`finalResult` ran');
+  $('.final').removeClass('hidden');
+  if (score<5) {
+    $('.final h1').text('Like this, you\'ll never make it to platinum!!');
+  } else if ((score>=5)&&(score<8)) {
+    $('.final h1').text('You need to watch more episodes of Cosmos!');
+  } else {
+    $('.final h1').text('You are ready for space exploration!');
+  }
+  $('.final span').text(score);
 }
 
-$(start);
+// this function will be our callback when the page loads. it's responsible for adding eventListeners.
+
+// $(document).ready(function(){
+  $(function() {
+
+//  When button in intro page is pressed, quiz starts and start page disapears.
+
+$("#js-launch").click(function(event) {
+    $('.start').hide();
+    renderQuestion();
+    });
+
+// relaunch quiz
+$('#js-relaunch').click(event=> {
+    event.preventDefault();
+    console.log('Play again button has been pressed');
+    $('.start').show();
+    $('.questions, .feedback').hide;
+    $('.final').addClass('hidden');
+    indexNumber =0;
+    score=0;
+
+  });
+
+// get the value of the selected answer
+  $('.questions').on('click','.option', event=> {
+    event.preventDefault();
+
+        // var selectedAnswerValue = $("input[class='option']:checked").val();
+    let selectedAnswerValue = $('.questions').find(event.currentTarget).attr('value');
+    console.log(`the selected item index is ${selectedAnswerValue}`);
+    evaluateAnswer(selectedAnswerValue);
+  });
+
+// call renderQuestion() when next button is pressed to create the next quiz item
+$('.feedback').on('click', '.next', event=> {
+    console.log("Next Question calling renderQuestion");
+    renderQuestion();
+    $('.feedback').hide();
+  });
+
+})
