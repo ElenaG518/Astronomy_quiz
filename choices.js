831f34ea836020d523f2f18af11a9bafec50a118
Elena Granados <patty518@yahoo.com>
2018-08-17 13:59:53 -0700

    modified choices.js

--- choices.js
+++ choices.js
@@ -7,7 +7,7 @@ const CHOICE = [
       "My wallet",
       "A place in space where gravity pulls so much that even light cannot get out.",
       "A dark place where all things that have ever been lost reside."],
-   correctAnswer: "2"
+   correctAnswer: 2
 },
 
  {
@@ -18,7 +18,7 @@ const CHOICE = [
       "A distant object powered by black holes a billion times as massive as our sun.",
       "A negatively charged quark that rotates the opposite way of everthing around it.",
       "Galaxies that are pulling objects toward them in order to swallow them whole."],
-   correctAnswer: "1"
+   correctAnswer: 1
  },
 
  {
@@ -29,7 +29,7 @@ const CHOICE = [
       "A distant object powered by black holes a billion times as massive as our sun.",
       "A place in space where gravity pulls so much that even light can not get out.",
       "A system of millions or billions of stars, together with gas and dust, held together by gravitational attraction."],
-   correctAnswer: "3"
+   correctAnswer: 3
  },
 
  {
@@ -40,7 +40,7 @@ const CHOICE = [
       "A satellite that orbits the sun.",
       "A large collection of gases and dust that orbits a gallaxy.",
       "A fixed luminous point in the night sky that is a large, remote incandescent body like the sun."],
-   correctAnswer: "0"
+   correctAnswer: 0
  },
 
  {
@@ -51,7 +51,7 @@ const CHOICE = [
       "A fixed luminous point in the night sky that is a large, remote incandescent body like the sun.",
       "A small body of matter from outer space that enters the earth's atmosphere, becoming incandescent as a result of friction and appearing as a streak of light.",
       "A round or near round object that orbits the sun, and which has revomed debris or other small objects from the area around its orbit."],
-   correctAnswer: "1"
+   correctAnswer: 1
  },
 
  {
@@ -62,7 +62,7 @@ const CHOICE = [
       "A theory that claims the universe originated billions of years ago in an explosion from a single point of nearly infinite energy density.",
       "A show about some cute astrophysicists who work at Caltech who have trouble dating because they are so smart.",
       "A theory that claims all living creatures originally evolved from bacteria."],
-   correctAnswer: "1"
+   correctAnswer: 1
  },
 
  {
@@ -73,7 +73,7 @@ const CHOICE = [
       "A fixed luminous point in the night sky that is a large, remote incandescent body like the sun.",
       "A star that suddenly increases greatly in brightness because of a catastrophic explosion that ejects most of its mass.",
       "A round or near round object that orbits the sun, and which has revomed debris or other small objects from the area around its orbit."],
-   correctAnswer: "2"
+   correctAnswer: 2
  },
 
  {
@@ -84,7 +84,7 @@ const CHOICE = [
       "A place in space where gravity pulls so much that even light can not get out.",
       "A form of energy which is hypothesized to permeate all of space, tending to accelerate the expansion of the universe, and which makes up nearly three-fourths of the universe.",
       "A dark place where all things that have ever been lost reside."],
-   correctAnswer: "0"
+   correctAnswer: 0
  },
 
  {
@@ -95,7 +95,7 @@ const CHOICE = [
       "An aspect of the Force, a metaphysical power that bound the galaxy together. The first known users of the dark side were the ancient Rakata, an alien species that conquered a large portion of the galaxy around 30,000 BBY.",
       "An energy at the center of black holes where gravity pulls so much that even light can not get out.",
       "A form of energy which is hypothesized to permeate all of space, tending to accelerate the expansion of the universe, and which makes up nearly three-fourths of the universe."],
-   correctAnswer: "3"
+   correctAnswer: 3
  },
 
  {
@@ -106,6 +106,6 @@ const CHOICE = [
       "is expanding at a decreasing rate.",
       "is contracting at an increasing rate.",
       "is contracting at a decreasing rate."],
-   correctAnswer: "0"
+   correctAnswer: 0
  }
 ]
